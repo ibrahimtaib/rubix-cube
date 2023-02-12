d583d594de226cba3f41abff8bcdd99084a9c637
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-02-12 12:31:29 +0100

    Enum Faces Simulating a C enum with values by using static final attributes instead of actual enum.

---
+++ main/Faces.java
@@ -0,0 +1,8 @@
+public final class Faces {
+    public final static int FRONT = 0;
+    public final static int RIGHT = 1;
+    public final static int BACK = 2;
+    public final static int LEFT = 3;
+    public final static int UP = 4;
+    public final static int BOTTOM = 5;
+}
